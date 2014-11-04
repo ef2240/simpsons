565ddebe8b7965289c38fb76b2f7d29bcbcde066
Eric Feder <efeder@theladders.com>
2014-11-04 11:35:26 -0500

    improve script cleaning

--- scripts/scrape_data.R
+++ scripts/scrape_data.R
@@ -12,11 +12,18 @@ html.code <- sapply(all.urls, getURL)
 html.code.split <- strsplit(html.code, "\n")
 scripts <- sapply(html.code.split, function(x) x[203])
 
+# Clean scripts
 cleanScript <- function(char){
-  cleaned <- gsub("\\t|\\r|\\[[[:alnum:]|[:space:]]*\\]|<br>", " ", char)
-  cleaned <- gsub("-|\\.|\\?|\"|\'\'|!", " ", cleaned)
-  cleaned <- gsub("[[:space:]]{2,}", " ", cleaned)
+  char <- iconv(char, "latin1", "ASCII", "")
+  end.intro <- regexpr("[ Screams ]", char, fixed=T)
+  char <- substring(char, end.intro + attr(end.intro, "match.length"))
+  cleaned <- gsub("\\t|\\r|\\[br\\]|<br>", " ", char, perl=T)
+  cleaned <- gsub("\\[.*?\\]", " ", cleaned, perl=T)
+  cleaned <- gsub("[[:punct:]]+ ", " ", cleaned, perl=T)
+  cleaned <- gsub("\"|\'\'", " ", cleaned, perl=T)
+  cleaned <- gsub("[[:space:]]{2,}", " ", cleaned, perl=T)
+  cleaned <- gsub("[[:blank:]]+$|^[[:blank:]]+", "", cleaned, perl=T)
   cleaned <- tolower(cleaned)
   return(cleaned)
 }
-gsub("\\[[[:alnum:]|[:space:]]*\\]", " ", "Homer, when you forgive someone[br]you can't throw it back at them like that.<br>\r \r Aw, what a gyp.<br>\r \r - Mmm.<br>[br]- Remember when I--\r \r - Homer![br]- Oh, yeah.<br> I forgot already.<br>\r \r    <br> [ Groans ]- Well, that's nothing,because you have a gambling problem!")
+scripts.clean <- cleanScript(scripts)
