0b58b41d1d4beec9d7aaf0a79e752735a93ca791
Eric Feder <efeder@theladders.com>
2014-11-05 11:03:26 -0500

    separate script for all data scraping

---
+++ scripts/scrape_data.R
@@ -0,0 +1,8 @@
+# Scrape and clean scripts
+source("scripts/scrape_scripts.R")
+episode.ids <- scrapeEpisodeIDs()
+episode.webpages <- scrapePages(episode.ids)
+scripts <- extractScripts(episode.webpages, episode.ids)
+
+# Save data
+save(episode.ids, episode.webpages, scripts, file="data/scripts.RData")
