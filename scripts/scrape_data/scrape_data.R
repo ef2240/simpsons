5a78d548dba6fc90ce23473b6836b04353d34ab1
Eric Feder <efeder@theladders.com>
2014-11-06 13:46:17 -0500

    remove missing scripts from vector

--- scripts/scrape_data/scrape_data.R
+++ scripts/scrape_data/scrape_data.R
@@ -4,5 +4,8 @@ episode.ids <- scrapeEpisodeIDs()
 episode.webpages <- scrapePages(episode.ids)
 scripts <- extractScripts(episode.webpages, episode.ids)
 
+# Remove missing scripts
+scripts <- scripts[!is.na(scripts)]
+
 # Save data
 save(episode.ids, episode.webpages, scripts, file="data/scripts.RData")
